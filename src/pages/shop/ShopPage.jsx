7a2d5e94e8889d805480f66fa121aec4ce775cc4
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-02 09:05:13 +0200

    uu id card

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -3,25 +3,53 @@ import { useEffect, useState } from 'react';
 import { localProductsUrl, productsUrl } from '../../config';
 import ShopListItem from '../../components/shop/ShopListItem';
 import { SiHomeassistantcommunitystore } from 'react-icons/si';
+import { v4 as genId } from 'uuid';
+
+const prodItemType = {
+  id: 30,
+  title: 'Key Holder',
+  description: 'Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality',
+  price: 30,
+  discountPercentage: 2.92,
+  rating: 4.92,
+  stock: 54,
+  brand: 'Golden',
+  category: 'home-decoration',
+  thumbnail: 'https://cdn.dummyjson.com/product-images/30/thumbnail.jpg',
+  images: [
+    'https://cdn.dummyjson.com/product-images/30/1.jpg',
+    'https://cdn.dummyjson.com/product-images/30/2.jpg',
+    'https://cdn.dummyjson.com/product-images/30/3.jpg',
+    'https://cdn.dummyjson.com/product-images/30/thumbnail.jpg',
+  ],
+};
 
 export default function ShopPage() {
   const [prodArr, setProdArr] = useState([]);
   const [cartArr, setCartArr] = useState([]);
 
   const cartObj = {
-    cItemId: 1,
+    cItemId: genId(),
     prodId: 1,
     title: 'Iphone',
     qty: 1,
-    price: 799,
+    priceUnit: 799,
+    img: 'blabla.jpg',
+    priceTotal: 799,
   };
 
   const addToCart = (itemId) => {
     // surati item is prodArr kurio id yra === itemId
     console.log('addToCard itemId ===', itemId);
+    const foundItem = prodArr.find((pObj) => pObj.id === itemId);
+    console.log('foundItem ===', foundItem);
     // suformuoti objekta (cartObj)
+    const madeObj = {};
+
+    console.log('madeObj ===', madeObj);
     // ideti objekta i cart cartArr (simple)
-    // jei jau yra toksai objektas carte - padidinti quantity
+
+    // jei jau yra toksai objektas carte - padidinti quantity ir kaina
   };
   const updateQtyCard = () => {
     // tures atnaujinti qty skaiciu kazkuriame objekte
@@ -54,7 +82,7 @@ export default function ShopPage() {
     <div className='container'>
       <h1 className='about-heading text-4xl font-bold text-center mt-10'>
         {' '}
-        <SiHomeassistantcommunitystore className='inline-block' size={'40px'} /> Shop
+        <SiHomeassistantcommunitystore className='inline-block' size={'40px'} /> Shop {genId()}
       </h1>
       <p className='text-lg text-center my-4'>
         This is just like <strong>Oboulys</strong> Shop
