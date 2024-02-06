e3ec7899a11d60f2de2bb4c3c262784ce57858d8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-06 09:14:16 +0200

    add images

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,9 @@
 export default function AboutPage() {
   return (
-    <div className='container'>
+    <div className='container text-center'>
       <h1 className='about-heading text-4xl font-bold text-center mt-10'>About</h1>
+      <img className='border p-4 h-52 inline-block' src='/person_6.jpg' alt='town' />
+
       <p className='text-lg text-center mt-4'>
         This is a demo app to showcase <strong>React</strong> and <strong>React Router</strong> in
         action.
